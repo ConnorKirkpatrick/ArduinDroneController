1f04c7a05aadc8137774298b2614fd91639f3a09
connor <ConnorKirkpatrick3@gmail.com>
2022-02-10 20:17:27 +0000

    Added function to add waypoint from right click location

---
+++ webServer/static/scripts/controls/newWayPointRightClick.js
@@ -0,0 +1,12 @@
+function newWaypointRightClick(coords){
+    //get earliest free id
+    let possibleIds = [1,2,3,4,5,6,7,8,9,10]
+    let usedIds = []
+    for (let i = 0; i < markerArray.length; i++) {
+        usedIds.push(markerArray[i].getId())
+    }
+    possibleIds = possibleIds.filter(n => !usedIds.includes(n))
+    let id = possibleIds[0]
+    addMarker(rightClickCoordinates[0],rightClickCoordinates[1],id)
+    addInterfaceWaypoint(id,rightClickCoordinates[0],rightClickCoordinates[1])
+}
--- webServer/static/scripts/mapInteractions/mapRightClick.js
+++ webServer/static/scripts/mapInteractions/mapRightClick.js
@@ -1,9 +1,8 @@
 const rightClickMenu = document.getElementById("rightClickMenu")
 map.getViewport().addEventListener("contextmenu", function (evt) {
     evt.preventDefault()
-    console.log(map.getEventCoordinate(evt))
+    rightClickCoordinates = ol.proj.transform(map.getEventCoordinate(evt),'EPSG:3857', 'EPSG:4326')
     const {clientX: mouseX, clientY: mouseY} = evt
-    console.log(mouseX,mouseY)
     rightClickMenu.style.top = `${mouseY}px`;
     rightClickMenu.style.left = `${mouseX}px`;
     rightClickMenu.classList.add("visible")
